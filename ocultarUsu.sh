36e387b1b12d7d804b3ca665662ce655b5ee2568
Alupop <115715419+Alupop@users.noreply.github.com>
2022-12-29 22:14:33 +0100

    Update ocultarUsu.sh

--- ocultarUsu.sh
+++ ocultarUsu.sh
@@ -4,8 +4,5 @@ usuarios=$(cat usuarios.csv | sed 1d)
 
 while read linea; do
         login=$(echo $linea | cut -d ',' -f1)
-        grupo_departamento=$(echo $linea | cut -d ',' -f5)
-        grupo_cuenta=$(echo $linea | cut -d ',' -f6)
-        sudo userdel "$login"
-        sudo groupdel "$grupo_departamento"
+        sudo usermod -L $login
 done <<< "$usuarios"
